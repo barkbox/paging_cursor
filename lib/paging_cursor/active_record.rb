8949cd01baf2348bdbc961f7935cbc9d46496a6e
lottetreg <cjtregelles@email.wm.edu>
2017-03-16 12:32:11 -0400

    try this

--- lib/paging_cursor/active_record.rb
+++ lib/paging_cursor/active_record.rb
@@ -18,7 +18,8 @@ module PagingCursor
         result.limit(options[:limit] || self.cursor_page_limit)
       end
 
-      def before(cursor=nil)
+      # default limit is not applied in before or after, only in cursor, fix README
+      def poop(cursor=nil)
         result = where(cursor ? arel_table[primary_key].lt(cursor) : nil).reorder(arel_table[primary_key].desc)
         result.sort_order = :desc
         result.cursored = true
@@ -30,6 +31,59 @@ module PagingCursor
         result.sort_order = :asc
         result.cursored = true
         result
+      end
+
+      def total_count(column_name = :all, _options = nil)
+        return @total_count if defined?(@total_count) && @total_count
+
+        # There are some cases that total count can be deduced from loaded records
+        if loaded?
+          # Total count has to be 0 if loaded records are 0
+          return @total_count = 0 if (current_page == 1) && @records.empty?
+          # Total count is calculable at the last page
+          per_page = (defined?(@_per) && @_per) || default_per_page
+          return @total_count = (current_page - 1) * per_page + @records.length if @records.any? && (@records.length < per_page)
+        end
+
+        # #count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
+        c = except(:offset, :limit, :order)
+        # Remove includes only if they are irrelevant
+        c = c.except(:includes) unless references_eager_loaded_tables?
+        # .group returns an OrderedHash that responds to #count
+        c = c.count(column_name)
+        @total_count = if c.is_a?(Hash) || c.is_a?(ActiveSupport::OrderedHash)
+          c.count
+        else
+          c.respond_to?(:count) ? c.count(column_name) : c
+        end
+      end
+    end
+
+    module Count
+
+      def total_count(column_name = :all, _options = nil) #:nodoc:
+        return @total_count if defined?(@total_count) && @total_count
+
+        # There are some cases that total count can be deduced from loaded records
+        if loaded?
+          # Total count has to be 0 if loaded records are 0
+          return @total_count = 0 if (current_page == 1) && @records.empty?
+          # Total count is calculable at the last page
+          per_page = (defined?(@_per) && @_per) || default_per_page
+          return @total_count = (current_page - 1) * per_page + @records.length if @records.any? && (@records.length < per_page)
+        end
+
+        # #count overrides the #select which could include generated columns referenced in #order, so skip #order here, where it's irrelevant to the result anyway
+        c = except(:offset, :limit, :order)
+        # Remove includes only if they are irrelevant
+        c = c.except(:includes) unless references_eager_loaded_tables?
+        # .group returns an OrderedHash that responds to #count
+        c = c.count(column_name)
+        @total_count = if c.is_a?(Hash) || c.is_a?(ActiveSupport::OrderedHash)
+          c.count
+        else
+          c.respond_to?(:count) ? c.count(column_name) : c
+        end
       end
     end
 
@@ -68,6 +122,10 @@ module PagingCursor
 
     ::ActiveRecord::Base.extend FinderMethods
     ::ActiveRecord::Base.extend Limit
+    ::ActiveRecord::Base.extend Count
+    # extend ActiveModelSerializers::Adapter::JsonApi by CursorPaginationLinks,
+    # monkey patch ActiveModelSerializers::Adapter::JsonApi, overload #pagination_links_for
+    # https://github.com/rails-api/active_model_serializers/blob/6c6e45b23f464bd0bb92ae05a4284b72b017be21/lib/active_model_serializers/adapter/json_api.rb
 
     klasses = [::ActiveRecord::Relation]
     if defined? ::ActiveRecord::Associations::CollectionProxy
@@ -80,6 +138,7 @@ module PagingCursor
     klasses.each do |klass| 
       klass.send(:prepend, SortedResults)
       klass.send(:include, FinderMethods) 
+      klass.send(:include, Count)
       klass.send(:include, ::PagingCursor::Direction)
     end
   end
--- lib/paging_cursor/pagination_links.rb
+++ lib/paging_cursor/pagination_links.rb
@@ -1,3 +1,16 @@
+module ActiveModel
+  class Serializer
+    class CollectionSerializer
+
+      def paginated?
+        ActiveModelSerializers.config.jsonapi_pagination_links_enabled &&
+          object.respond_to?(:cursor_page_limit) &&
+          object.respond_to?(:total_count)
+      end
+    end
+  end
+end
+
 module ActiveModelSerializers
   module Adapter
     class JsonApi < Base
@@ -20,9 +33,11 @@ module ActiveModelSerializers
         end
 
         def as_json
-          per_page = collection.try(:per_page) || collection.try(:limit_value) || collection.size
+          per_page = collection.cursor_page_limit
           pages_from.each_with_object({}) do |(key, value), hash|
-            params = query_parameters.merge(page: { number: value, size: per_page }).to_query
+            # params = query_parameters.merge(page: { number: value, size: per_page }).to_query
+            # params = query_parameters.merge(cursor: { direction => max_index }).to_query # make sure removing old cursor params
+            params = query_parameters.merge(cursor: { value }).to_query # make sure removing old cursor params
 
             hash[key] = "#{url(adapter_options)}?#{params}"
           end
@@ -36,18 +51,25 @@ module ActiveModelSerializers
 
         def pages_from
           return {} if collection.total_pages <= FIRST_PAGE
+          # return {} if collection.total_size <= collection.cursor_page_limit
 
           {}.tap do |pages|
             pages[:self] = collection.current_page
+            # pages[:self] = { before: collection.cursor_after }
 
-            unless collection.current_page == FIRST_PAGE
+            unless collection.current_page == FIRST_PAGE # unless on first page
+            # unless collection.cursor_after == collection.size # unless on first page
               pages[:first] = FIRST_PAGE
+              # pages[:first] = { direction => max_index }
               pages[:prev]  = collection.current_page - FIRST_PAGE
+              # pages[:prev] = { direction => max_index }
             end
 
-            unless collection.current_page == collection.total_pages
+            unless collection.current_page == collection.total_pages # unless on last page
               pages[:next] = collection.current_page + FIRST_PAGE
+              # pages[:next] = { direction => max_index }
               pages[:last] = collection.total_pages
+              # pages[:last] = { direction => max_index }
             end
           end
         end
